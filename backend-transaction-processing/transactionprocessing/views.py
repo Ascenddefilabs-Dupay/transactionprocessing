bfd78bc1ffd02f8b248fb8d12ea6257f9dc4d1dc
Ganesh-Mirashi <ganesh.mirashi@ascenddefi.com>
2024-08-29 17:39:57 +0530

    added backend structure in dupayfrontend

--- backend-transaction-processing/transactionprocessing/views.py
+++ backend-transaction-processing/transactionprocessing/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Project
 from .serializers import ProjectSerializer
+
+
 # Create your views here.
 
 class ProjectViewSet(viewsets.ModelViewSet):
